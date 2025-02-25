b3279be6bef7e2d2e2dfc1853f9d05fb8d621a94
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2025-02-25 15:00:40 +0000

    feat(winner): add functionality to check who is the winner

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -27,6 +27,16 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,6],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
 
 function App() {
@@ -36,13 +46,45 @@ const [board, setBoard] = useState(Array(9).fill(null))
 
 const [turn, setTurn] = useState(TURNS.X) // creamos otro estado para saber quien tiene el turno.
 
+const [winner, setWinner] = useState(null) // null es que no hay ganador, false es que hay un empate, true que hay ganador
+
+
+const checkWinner = (boardToCheck) => {
+  //revisamos todas las combinaciones ganadoras para ver si ganó X o O
+  for(const combo of WINNER_COMBOS){
+    const [a,b,c] = combo
+    if(
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ){
+      return boardToCheck[a]
+    }
+  }
+  return null
+}
+
+
+
+
 const updateBoard = (index) =>{
+
+  /*No actualizamos esta posición si ya tiene algo */
+if(board[index] || winner) return
+// Actualizar el tablero
   const newBoard =[...board]
   newBoard[index] = turn
   setBoard(newBoard)
-
+//cambiar el turno
   const newTurn = turn === TURNS.X ? TURNS.O :TURNS.X
   setTurn(newTurn)
+  //revisas si hay ganador
+  const newWinner = checkWinner(newBoard)
+  if(newWinner){
+    setWinner(newWinner)
+  } //TODO: check if game is over
+
+
 }
 
   return (
