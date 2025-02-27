56b207fb2be132b86a2c86bec7ac1b578476bbd1
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2025-02-27 08:58:57 +0000

    feat(button): added button to enable following mouse pointer

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import { useEffect, useState } from "react";
 
+function App() {
+  const [enable, setEnable] = useState(false);
+
+  useEffect(() => {
+    console.log("efecto",{enable});
+  },[enable]);
 
   return (
     <>
       <h3>Proyecto 2</h3>
+      <button onClick={()=>setEnable(!enable)}>
+        {enable ? "Desactivar" : "Activar"} seguir puntero
+      </button>
     </>
   );
 }
